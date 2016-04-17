0a87fcd44256cbb2e4b9523cfb861bfc90beb255
Costa Shapiro <costa@mouldwarp.com>
2016-04-17 15:59:27 +0300

    replaced text input with firebase; came back to the official dagre-d3 bower package; fixed script component dependencies; cleaned/styled the UI up a bit

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,40 +1,8 @@
+// NOTE heavily based on https://github.com/cpettitt/dagre-d3/blob/master/demo/interactive-demo.html
+
+var TRANSITION_DURATION_MS = 500;
+
 $(function() {
-
-  // TODO a shameless rip off dagre-d3 interactive demo
-
-  // Input related code goes here
-
-  function graphToURL() {
-    var elems = [window.location.protocol, '//',
-                 window.location.host,
-                 window.location.pathname,
-                 '?'];
-
-    var queryParams = [];
-    if (debugAlignment) {
-      queryParams.push('alignment=' + debugAlignment);
-    }
-    queryParams.push('graph=' + encodeURIComponent(inputGraph.value));
-    elems.push(queryParams.join('&'));
-
-    return elems.join('');
-  }
-
-  var inputGraph = document.querySelector("#inputGraph");
-
-  var graphLink = d3.select("#graphLink");
-
-  var oldInputGraphValue;
-
-  var graphRE = /[?&]graph=([^&]+)/;
-  var graphMatch = window.location.search.match(graphRE);
-  if (graphMatch) {
-    inputGraph.value = decodeURIComponent(graphMatch[1]);
-  }
-  var debugAlignmentRE = /[?&]alignment=([^&]+)/;
-  var debugAlignmentMatch = window.location.search.match(debugAlignmentRE);
-  var debugAlignment;
-  if (debugAlignmentMatch) debugAlignment = debugAlignmentMatch[1];
 
   // Set up zoom support
   var svg = d3.select("svg"),
@@ -48,38 +16,64 @@ $(function() {
   // Create and configure the renderer
   var render = dagreD3.render();
 
-  function tryDraw() {
-    var g;
-    if (oldInputGraphValue !== inputGraph.value) {
-      inputGraph.setAttribute("class", "");
-      oldInputGraphValue = inputGraph.value;
-      try {
-        g = graphlibDot.read(inputGraph.value);
-      } catch (e) {
-        inputGraph.setAttribute("class", "error");
-        throw e;
-      }
+  var oldGraph;
 
-      // Save link to new graph
-      graphLink.attr("href", graphToURL());
-
-      // Set margins, if not present
-      if (!g.graph().hasOwnProperty("marginx") &&
-          !g.graph().hasOwnProperty("marginy")) {
-        g.graph().marginx = 20;
-        g.graph().marginy = 20;
-      }
-
-      g.graph().transition = function(selection) {
-        return selection.transition().duration(500);
-      };
-
-      // Render the graph into svg g
-      d3.select("svg g").call(render, g);
+  function tryDraw(graph) {
+    if (oldGraph == graph) {
+      return;
     }
+
+    // inputGraph.setAttribute("class", "");
+    var g;
+    try {
+      g = graphlibDot.read(graph);
+    } catch (e) {
+      // inputGraph.setAttribute("class", "error");
+      throw e;
+    }
+
+    // Set margins, if not present
+    if (!g.graph().hasOwnProperty("marginx") &&
+        !g.graph().hasOwnProperty("marginy")) {
+      g.graph().marginx = 20;
+      g.graph().marginy = 20;
+    }
+
+    g.graph().transition = function(selection) {
+      return selection.transition().duration(TRANSITION_DURATION_MS);
+    };
+
+    // Render the graph into svg g
+    d3.select("svg g").call(render, g);
+
+    oldGraph = graph;
   }
 
-  tryDraw();
-  $('title', document).text("Viewing process #YYYYYY");
+  var pidRE = /[?&]pid=([^&]+)/;
+  var pidMatch = window.location.search.match(pidRE);
+  var pid = pidMatch && decodeURIComponent(pidMatch[1]);
 
+  if (!pid) {
+    return window.location.pathname = '/?pid=' + prompt('PID');
+  }
+
+  var procFB = new Firebase("https://proc-vis-io.firebaseio.com/").child('proc');
+
+  function digraphy(dotData) {
+    return "digraph {\n" +
+      "node [rx=5 ry=5 labelStyle=\"font: 300 14px 'Helvetica Neue', Helvetica\"]\n" +
+      "edge [labelStyle=\"font: 300 14px 'Helvetica Neue', Helvetica\"]\n" +
+      dotData + "\n}"
+  }
+
+  procFB.child(pid).on('value', function(snapshot) {
+    var data = snapshot && snapshot.val();
+    var dotData = data && data.dot;
+    if (!dotData) {
+      dotData = "ZZZ [labelType=\"html\" label=\"<big>Waiting to hear from pid " + pid + "</big>\"];"
+    }
+    tryDraw(digraphy(dotData));
+  });
+
+  $('title', document).text("Viewing process #" + pid);
 });
